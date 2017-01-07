a1fee3e889dbba92503f8a1944ab1f16af88c664
Jared Dalessandro <jareddalessandro1@gmail.com>
2017-01-07 14:13:25 -0700

    Add files via upload

---
+++ Hours2/Hours2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hours2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pay = 0;
+            double rte = 0;
+            double hrs = 0;
+
+            // Gets hours and rate to output pay
+            Console.WriteLine("Enter Hours: ");
+            string hours = Console.ReadLine();
+
+            Console.WriteLine("Enter Rate: ");
+            string rate = Console.ReadLine();
+
+            hrs = Convert.ToDouble(hours);
+            rte = Convert.ToDouble(rate);
+
+            // Takes into consideration overtime at x1.5 after 40 hrs
+            if(hrs <= 40)
+            {
+                pay = hrs * rte;
+            }
+            else
+            {
+                pay = rte * 40 + (rte * 1.5 * (hrs - 40));
+            }
+           
+            Console.WriteLine();
+            Console.WriteLine("Your payment is: ${0}", pay);
+            Console.ReadLine();
+
+        }
+    }
+}
