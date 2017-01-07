9fe8d39d838894e619d996ad2c2e047a80913f9e
Jared Dalessandro <jareddalessandro1@gmail.com>
2017-01-07 16:10:51 -0700

    Add files via upload

---
+++ Hours5/Hours5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hours5
+{
+    class Program
+    {
+        static void Payment(double hours, double rate)
+        {
+            double pay = 0;
+
+            // Takes into consideration overtime at x1.5 after 40 hrs
+            if (hours <= 40)
+            {
+                pay = hours * rate;
+            }
+            else
+            {
+                pay = rate * 40 + (rate * 1.5 * (hours - 40));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Your payment is: ${0}", pay);
+            Console.ReadLine();
+        }
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // Gets hours and rate to output pay
+                Console.WriteLine("Enter Hours: ");
+                string hrs = Console.ReadLine();
+
+                Console.WriteLine();
+
+                Console.WriteLine("Enter Rate: ");
+                string rte = Console.ReadLine();
+
+                // Declare variables
+                double rate = 0;
+                double hours = 0;
+                // Convert to string input to double
+                try
+                {
+                    hours = Convert.ToDouble(hrs);
+                    rate = Convert.ToDouble(rte);
+                }
+                catch
+                {
+                    Console.WriteLine("---- Invalid Input ----");
+                    Console.ReadLine();
+                }
+
+                Payment(hours, rate);
+            }
+        }
+    }
+}
