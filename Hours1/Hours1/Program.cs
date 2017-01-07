81764a2b1e63bc1b17e96ca407391e1c4c8e01a5
Jared Dalessandro <jareddalessandro1@gmail.com>
2017-01-07 14:12:40 -0700

    Add files via upload

---
+++ Hours1/Hours1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hours1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pay = 0;
+            double rte = 0;
+            double hrs = 0;
+
+            // Gets hours and rate to output pay
+            Console.WriteLine("Enter Hours: ");
+            string hours = Console.ReadLine();
+
+            Console.WriteLine("Enter Rate: ");
+            string rate = Console.ReadLine();
+
+            hrs = Convert.ToDouble(hours);
+            rte = Convert.ToDouble(rate);
+
+            pay = rte * hrs;
+
+            Console.WriteLine();
+            Console.WriteLine("Your payment is: ${0}", pay);
+            Console.ReadLine();
+
+        }
+    }
+}
